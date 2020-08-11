2494b30152750c77fe1bc54a1b4012bfefb81acf
rathaurh <66039613+rathaurh@users.noreply.github.com>
2020-08-11 08:39:35 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,48 +1,149 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
-package ca.sheridancollege.project;
+
+package blackjack;
 
 /**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
+ *
+ * @author Harjeet Kaur Rathaur
  */
-public abstract class Player 
+import java.util.ArrayList;
+import java.util.Arrays;
+/*
+ * it is a dealer class along with which the dealer plays the cards
+ */
+class Dealer {
+ArrayList<Card> hand;//dealers hand
+private int valueInHand=0;// the value contain by the hand of the dealer
+private Card[] dealerHand;// changes the hand in the arraylist
+private int AceCount;//numbers of Aces in the hand of the dealer
+Dealer(GroupOfCards deckes)
 {
-    private String playerID; //the unique ID for this player
-    
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name)
+     int AceCount=0;
+    hand = new ArrayList<>();
+    dealerHand = new Card[]{};
+   
+    for(int a=0; a<2; a++)
     {
-        playerID= name;
+        hand.add(deckes.drawingACard());
     }
     
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
+    dealerHand = hand.toArray(dealerHand);
+    for(int b=0; b<dealerHand.length; b++)
     {
-        return playerID;
+        valueInHand += dealerHand[b].getValue();
+        if(dealerHand[b].getValue()==11)
+        {
+            AceCount++;
+        }
+        while(AceCount>0 && valueInHand>21)
+        {
+            valueInHand-=10;
+            AceCount--;
+        }
     }
+}
 
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
+    
+/*
+ * display whats in the dealers first hand
+ */
+public void dealerFirstcard()
+{
+    Card[] firstCard = new Card[]{};
+    firstCard = hand.toArray(firstCard);
+    System.out.println("("+firstCard[0]+")");
+}
+/*
+ * hits the dealer with other card and increase its value.
+ */
+public void Hitting(GroupOfCards deckes)
+{
+        valueInHand = 0;
+    hand.add(deckes.drawingACard());
+    dealerHand = hand.toArray(dealerHand);
+
+    for(int z=0; z<dealerHand.length; z++)
     {
-        playerID = givenID;
+        valueInHand = dealerHand[z].getValue() + valueInHand;
+        if(dealerHand[z].getValue()==11)
+        {
+            AceCount++; //number of ace in the hand of the dealer
+        }
+        while(AceCount>0 && valueInHand>21)
+        {
+            valueInHand-=10;
+            AceCount--;
+        }
     }
-    
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
-    
+}
+
+/*
+ * Only true if the dealer is having the blackjack card
+ */
+public boolean haveBlack()
+{
+    if(hand.size()==2 && valueInHand==21)
+    {
+        System.out.println("---------- Dealer is having the Blackjack card----------------");
+        return true;
+    }
+    return false;
+}
+/*
+ * Displays the dealer hands
+ */
+public void showDealerHand()
+{
+    System.out.println(" "+hand+" ");//shows cards in the hand
+}
+
+/*
+ * Determines if a dealer has busted.
+ */
+public boolean dealerBusted(int valueInHand)
+{
+    if(valueInHand>21)
+    {
+        System.out.println("-+-+-+The Dealer got busted!!!!!!-+-+-+");
+        return true;
+    }
+    return false;
+}
+/*
+ * Takes the turn for the dealer and returns the value of his hand.
+ */
+public int takeDealerTurn(GroupOfCards deckes)
+{
+    while(dealerWantedToHint())
+    {
+        System.out.println("******Dealer is hitting******");
+        Hitting(deckes);
+        if(dealerBusted(valueInHand))
+        {
+            break;
+        }
+    }
+    if(valueInHand<=21)
+    {
+        System.out.print("o0o0o0odealer still stands00o0o0o0o");
+    }
+    return valueInHand;
+}
+/*
+ * if dealer wanted a hint
+ */
+public boolean dealerWantedToHint()
+{
+    if(valueInHand<17)
+    {
+        return true; //return true if he wanted hint
+    }
+    return false;
+}
+/*
+ * getting value of the dealers hand
+ */
+public int getvalueInHand()
+{
+    return valueInHand; //returns the cards in the hand
+}
 }
